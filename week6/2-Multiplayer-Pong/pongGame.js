3e3798326a119f2e8ccaaf45c7c3e4f014cb051e
Viktor Staikov <viktor.staikov@telerik.com>
2014-07-22 15:56:59 +0300

    some inprovements in game controls and some regretion when bouncing

--- week6/2-Multiplayer-Pong/pongGame.js
+++ week6/2-Multiplayer-Pong/pongGame.js
@@ -1,6 +1,6 @@
-'use string';
+'use strict';
 
-var point = function(x, y) {
+var point = function (x, y) {
   this.x = x || 0;
   this.y = y || 0;
 
@@ -9,19 +9,21 @@ var point = function(x, y) {
 
     this.x = this.x / length;
     this.y = this.y / length;
-  }
+  };
 };
 
-var player = function(ctx, side, color, opt) {
-  if (!opt) opt = {};
+var player = function (ctx, playerSide, playerColor, opt) {
+  if (!opt) {
+    opt = {};
+  }
 
   var
     width = opt.width || 20,
     height = opt.height || 80,
     offset = opt.offset || 20,
-    speed = opt.speed || 20,
-    side = side,
-    color = color || 'blue';
+    speed = opt.speed || 10,
+    side = playerSide,
+    color = playerColor || 'blue';
 
   var position;
   if (side === 'left') {
@@ -30,18 +32,20 @@ var player = function(ctx, side, color, opt) {
     position = new point(ctx.canvas.width - offset - width / 2, ctx.canvas.height / 2);
   }
 
-  function initControls() {
-    $(document).keydown(function(event) {
-      // if (event.keyCode === 39 && direction != "left") {
-      //   direction = "right";
-      // }
+  function move(dir, dist) {
+    var travelled = dist || speed;
+    if (dir === 'up') {
+      position.y -= travelled;
+    }
+    if (dir === 'down') {
+      position.y += travelled;
+    }
+  }
 
-      // if (event.keyCode === 37 && direction != "right") {
-      //   direction = "left";
-      // }
+  function initControls() {
+    $(document).keydown(function (event) {
 
       if (event.keyCode === 38) {
-        //direction = "up";
         move('up');
       }
 
@@ -49,15 +53,16 @@ var player = function(ctx, side, color, opt) {
         move('down');
       }
     });
-  }
 
-  function move(dir) {
-    if (dir === 'up') {
-      position.y -= speed;
-    }
-    if (dir === 'down') {
-      position.y += speed;
-    }
+
+    var _lastPos = position;
+    $(document).mousemove(function (e) {
+
+      var dir = (e.offsetY - _lastPos.y < 0) ? 'up' : 'down';
+
+      move(dir, Math.abs(e.offsetY - _lastPos.y));
+      _lastPos.y = e.offsetY;
+    });
   }
 
   function print() {
@@ -72,18 +77,20 @@ var player = function(ctx, side, color, opt) {
     position: position,
     width: width,
     height: height
-  }
+  };
 };
 
-var ball = function(ctx, opt) {
-  if (!opt) opt = {};
+var ball = function (ctx, opt) {
+  if (!opt) {
+    opt = {};
+  }
 
   var
     radius = opt.radius || 5,
     speed = opt.speed || 5,
     color = opt.color || 'yellow',
     position = new point(ctx.canvas.width / 2, ctx.canvas.height / 2),
-    direction = new point(Math.random(), Math.random()),
+    direction = new point(Math.random() * 0.6 + 0.3, Math.random()),
     moveInterval;
 
   direction.normalize();
@@ -96,31 +103,45 @@ var ball = function(ctx, opt) {
   function print() {
     ctx.beginPath();
     ctx.arc(position.x, position.y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.stroke();
   }
 
+  function bounceSmart(normal) {
+    // reference: http://stackoverflow.com/questions/3306838/algorithm-for-reflecting-a-point-across-a-line
+    var
+      x = direction.x,
+      y = direction.y,
+      a = normal.x,
+      c = normal.y;
+    var d = (x + (y - c) * a) / (1 + a * a);
+
+    direction.x = 2 * d - x;
+    direction.y = 2 * d * a - y + 2 * c;
+  }
+
   function bounce(normal) {
-    if (normal.x === 1 && normal.y === 0) {
+    if (Math.abs(normal.x) === 1 && normal.y === 0) {
       direction.x = -direction.x;
-    }
-    if (normal.x === 0 && normal.y === 1) {
+    } else if (normal.x === 0 && Math.abs(normal.y) === 1) {
       direction.y = -direction.y;
+    } else {
+      bounceSmart(normal);
     }
   }
 
   return {
-    startMoving: function() {
+    startMoving: function () {
       moveInterval = setInterval(function moving() {
         move();
-        if (position.x <= radius || position.x >= ctx.canvas.width - radius) {
+        if (position.y >= ctx.canvas.height - radius) {
           bounce({
-            x: 1,
-            y: 0
+            x: 0,
+            y: -1
           });
         }
-        if (position.y <= radius || position.y >= ctx.canvas.height - radius) {
+        if (position.y < radius) {
           bounce({
             x: 0,
             y: 1
@@ -128,7 +149,7 @@ var ball = function(ctx, opt) {
         }
       }, 10);
     },
-    stopMoving: function() {
+    stopMoving: function () {
       clearInterval(moveInterval);
     },
     bounce: bounce,
@@ -139,18 +160,57 @@ var ball = function(ctx, opt) {
   };
 };
 
-var pongGame = function(ctx) {
+var pongGame = function (ctx) {
   var player1 = new player(ctx, 'left', 'red');
   var player2 = new player(ctx, 'right', 'blue');
   var b = new ball(ctx);
   var interval;
+
+  function end(winner) {
+    clearInterval(interval);
+    var msg = 'player ' + winner + ' wins!';
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    var
+      maxWidth = 100,
+      tHeight = 40;
+
+    ctx.fillStyle = "blue";
+    ctx.font = "bold " + tHeight + "px Arial";
+    ctx.fillText(msg, ctx.canvas.width / 2 - maxWidth / 2, ctx.canvas.height / 2 - tHeight / 2, maxWidth);
+  }
+
+  function checkForCollision() {
+    var n;
+
+    if (b.position.x < player1.position.x + player1.width / 2) {
+      if (Math.abs(b.position.y - player1.position.y) <= player1.height) {
+        n = new point(ctx.canvas.width / 2 - player1.position.x, ctx.canvas.height / 2 - player1.position.y);
+        n.normalize();
+        b.bounce(n);
+      } else {
+        end(2);
+      }
+    }
+
+    if (b.position.x > player2.position.x - player2.width / 2) {
+      if (Math.abs(b.position.y - player2.position.y) <= player2.height) {
+        n = new point(ctx.canvas.width / 2 - player2.position.x, ctx.canvas.height / 2 - player2.position.y);
+        n.normalize();
+        b.bounce(n);
+      } else {
+        end(1);
+      }
+    }
+  }
 
   function start() {
     player1.init();
     player2.init();
     b.startMoving();
 
-    interval = setInterval(function() {
+    interval = setInterval(function () {
       ctx.fillStyle = 'white';
       ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -161,50 +221,9 @@ var pongGame = function(ctx) {
       checkForCollision();
 
     }, 10);
-
-    function end(winner) {
-      clearInterval(interval);
-      msg = 'player ' + winner + ' wins!';
-
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-      // alert(msg);
-
-      var
-        maxWidth = 100,
-        tHeight = 40;
-
-      ctx.fillStyle = "blue";
-      ctx.font = "bold " + tHeight + "px Arial";
-      ctx.fillText(msg, ctx.canvas.width / 2 - maxWidth / 2, ctx.canvas.height / 2 - tHeight / 2, maxWidth);
-    }
-
-    function checkForCollision() {
-
-      if (b.position.x < player1.position.x + player1.width / 2) {
-        if (Math.abs(b.position.y - player1.position.y) <= player1.height) {
-          b.bounce({
-            x: 1,
-            y: 0
-          });
-        } else {
-          end(2);
-        }
-      }
-
-      if (b.position.x > player2.position.x - player2.width / 2) {
-        if (Math.abs(b.position.y - player2.position.y) <= player2.height) {
-          b.bounce({
-            x: 1,
-            y: 0
-          });
-        } else {
-          end(1);
-        }
-      }
-    }
   }
+
   return {
     start: start
-  }
+  };
 };
